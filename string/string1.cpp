e86e46341a351011c68e9da91e08941537d51087
jinkyu <wlsrb8993@gmail.com>
2018-01-23 21:22:02 +0900

    string class

---
+++ string/string1.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+
+	string str1="Hello ";
+	string str2="world!";
+
+	cout<<"str1 : "<<str1<<endl;
+	cout<<"str2 : "<<str2<<endl;
+
+	string str3=str1+str2;
+	cout<<"str1+str2 : "<<str3<<endl;
+
+	string str4;
+	cout<<"문자열 입력 : ";
+	cin>>str4;
+
+	cout<<"입력한 문자열 : "<<str4<<endl;
+
+
+
+
+
+	return 0;
+}
